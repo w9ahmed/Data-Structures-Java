3a801e7562e8eb0bea8cbb39cb2d35fc10cbb6c9
Ahmed Sami Mohamed <asyn.nile@gmail.com>
2016-03-11 09:45:12 +0100

    Update Singly LinkedList.java

--- Singly LinkedList.java
+++ Singly LinkedList.java
@@ -5,10 +5,12 @@ public class IntSLLNode {
 	public int info;
 	public IntSLLNode next;
 	
+	// constructor
 	public IntSLLNode(int i) {
 		this(i, null);
 	}
 	
+	// constructor_1
 	public IntSLLNode(int i, IntSLLNode n) {
 		info = i;
 		next = n;
